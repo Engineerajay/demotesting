defe0cbc559614613ecf6753a3327a491f19615b
Ajay <ajayingole44@gmail.com>
2021-10-23 16:40:56 +0530

    this is second file added

---
+++ Testingdemo/src/ajay/git2.java
@@ -0,0 +1,10 @@
+package ajay;
+
+public class git2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is second class in ajay");
+	}
+
+}
