e1db0984cef0f8840d2b58c25d9b95932096c148
Ajay <ajayingole44@gmail.com>
2021-10-23 16:46:34 +0530

    git3 file added in git

---
+++ Testingdemo/src/ajay/git3.java
@@ -0,0 +1,10 @@
+package ajay;
+
+public class git3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("demo practice for adding file in git");
+	}
+
+}
